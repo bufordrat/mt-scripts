986009f1be7b45624f904970bb7bfa926dad0dc7
Matt Teichman <teichman@uchicago.edu>
2024-10-29 10:25:51 -0500

    various changes that I forgot

--- figure_out_editor_variable.sh
+++ figure_out_editor_variable.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 if echo "$INSIDE_EMACS" | grep -q tramp
-then echo "no \$EDITOR over an ssh-tramp connection" ; exit 1
-# add 'and inside emacs doesn't contain tramp conunction to this elif
-elif [ "$TERM" = ansi ]
-then echo "no \$EDITOR when \$TERM is 'ansi'" ; exit 1
-elif [ -n "$INSIDE_EMACS" -o 'emacs --version > /dev/null' ]
-then if [ -n "$TERM" ]
-     then emacsclient -nw -a 'emacs -nw' "$@"
-     else emacsclient -a emacs "$@"
+then echo "no \$EDITOR over a tramp connection" ; exit 1
+elif [ -z "$INSIDE_EMACS" -a "$TERM" = ansi ]
+then echo "no \$EDITOR in an ssh-ed Emacs shell" ; exit 1
+elif [ -n "$INSIDE_EMACS" ] || emacs --version > /dev/null
+# fix this below
+then if [ "I'm in a real terminal" ]
+     then emacsclient -a emacs "$@"
+     else emacsclient -nw -a 'emacs -nw' "$@"
      fi
 else vim "$@" || vi "$@" || echo 'no available $EDITOR in this terminal' 1>&2
 fi
