e1edd159fe63fb97a35ca374b9c64c2220a3cf10
Matt Teichman <teichman@uchicago.edu>
2024-10-29 11:06:24 -0500

    fleshed out the conditional logic further

--- figure_out_editor_variable.sh
+++ figure_out_editor_variable.sh
@@ -1,15 +1,38 @@
 #!/bin/sh
 set -x
 
+
 if echo "$INSIDE_EMACS" | grep -q tramp
-then echo "no \$EDITOR over a tramp connection" ; exit 1
+then # we are in an Emacs shell launched from a tramped location
+    echo "no \$EDITOR over a tramp connection" ; exit 1
 elif [ -z "$INSIDE_EMACS" -a "$TERM" = ansi ]
-then echo "no \$EDITOR in an ssh-ed Emacs shell" ; exit 1
-elif [ -n "$INSIDE_EMACS" ] || emacs --version > /dev/null
-# fix this below
-then if [ "I'm in a real terminal" ]
-     then emacsclient -a emacs "$@"
-     else emacsclient -nw -a 'emacs -nw' "$@"
-     fi
-else vim "$@" || vi "$@" || echo 'no available $EDITOR in this terminal' 1>&2
+then # we are in an Emacs shell that ssh-ed into a server from sequent
+    echo "no \$EDITOR in an ssh-ed Emacs shell" ; exit 1
+elif [ -n "$INSIDE_EMACS" ]
+then # we're in a local Emacs shell i.e. not sshed/tramped in
+    if emacsclient --eval '(display-graphic-p)'
+    then # the local Emacs shell is an -nw emacs
+	if emacsclient --eval '(princ :ok)'
+	then # there is an Emacs server running
+	    emacsclient -nw -a 'emacs -nw' "$@"
+	else # there is no Emacs server running
+	    emacs -nw "$@"
+	fi
+    else #the local Emacs shell is graphical
+	emacsclient -a emacs "$@"
+    fi
+else # we are in a straight terminal and possibly ssh-ed in somewhere
+    if emacs --version > /dev/null
+    then emacsclient -nw -a 'emacs -nw' "$@"
+    else vim "$@" || vi "$@" || echo 'no available $EDITOR in this terminal' 1>&2
+    fi
 fi
+
+# elif [ -n "$INSIDE_EMACS" ] || emacs --version > /dev/null
+# # fix this below
+# then if [ "I'm in a real terminal" ]
+#      then emacsclient -a emacs "$@"
+#      else emacsclient -nw -a 'emacs -nw' "$@"
+#      fi
+# else vim "$@" || vi "$@" || echo 'no available $EDITOR in this terminal' 1>&2
+# fi
