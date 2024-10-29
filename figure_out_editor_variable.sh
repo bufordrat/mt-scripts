73092ee53d917c6b47f605d42d575b3a191a664e
Matt Teichman <teichman@uchicago.edu>
2024-10-28 22:00:34 -0500

    delete commented-out stuff

--- figure_out_editor_variable.sh
+++ figure_out_editor_variable.sh
@@ -8,18 +8,3 @@ elif [ -n "$INSIDE_EMACS" -o 'emacs --version > /dev/null' ]
 then emacsclient -a emacs "$@"
 else vim "$@" || vi "$@" || echo 'no available $EDITOR in this terminal' 1>&2
 fi
-
-# else
-# fi
-
-# if [ -n "$INSIDE_EMACS" -o "$TERM" = ansi ]
-# then if emacs --version > /dev/null
-#      then emacsclient -a emacs "$@"
-#      else echo 'no available $EDITOR; are you in an Emacs shell?'; exit 1
-# 	  # echo Message here; read msg; echo "$msg" > "$1"
-#      fi
-# else if emacs --version > /dev/null
-#      then emacsclient -nw -a 'emacs -nw' "$@"
-#      else vim "$@" || vi "$@" || echo 'no available $EDITOR in this terminal' 1>&2
-#      fi
-# fi
