3d20a6067d245df1c1b8d83b7ee241a9e1c61a24
Matt Teichman <teichman@uchicago.edu>
2024-10-28 22:06:03 -0500

    oof, nested if for -nw sitch

--- figure_out_editor_variable.sh
+++ figure_out_editor_variable.sh
@@ -5,6 +5,9 @@ then echo "no \$EDITOR over an ssh-tramp connection" ; exit 1
 elif [ "$TERM" = ansi ]
 then echo "no \$EDITOR when \$TERM is 'ansi'" ; exit 1
 elif [ -n "$INSIDE_EMACS" -o 'emacs --version > /dev/null' ]
-then emacsclient -a emacs "$@"
+then if [ -n "$TERM" ]
+     then emacsclient -nw -a emacs -nw "$@"
+     else emacsclient -a emacs "$@"
+     fi
 else vim "$@" || vi "$@" || echo 'no available $EDITOR in this terminal' 1>&2
 fi
