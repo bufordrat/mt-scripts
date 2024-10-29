7854963d62cb545cb38b5e473648e7079737568c
Matt Teichman <teichman@uchicago.edu>
2024-10-28 22:19:01 -0500

    forgot quotes around the -a options

--- figure_out_editor_variable.sh
+++ figure_out_editor_variable.sh
@@ -6,8 +6,8 @@ elif [ "$TERM" = ansi ]
 then echo "no \$EDITOR when \$TERM is 'ansi'" ; exit 1
 elif [ -n "$INSIDE_EMACS" -o 'emacs --version > /dev/null' ]
 then if [ -n "$TERM" ]
-     then emacsclient -nw -a emacs -nw "$@"
-     else emacsclient -a emacs "$@"
+     then emacsclient -nw -a 'emacs -nw "$@"'
+     else emacsclient -a 'emacs "$@"'
      fi
 else vim "$@" || vi "$@" || echo 'no available $EDITOR in this terminal' 1>&2
 fi
